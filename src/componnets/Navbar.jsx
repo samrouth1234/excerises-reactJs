ca165771c7ca88c5d7e576f99ce68330a69f1ac9
bich samrouth <125858166+samrouth1234@users.noreply.github.com>
2024-06-08 07:36:34 -0100

    Update Navbar.jsx

--- src/componnets/Navbar.jsx
+++ src/componnets/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
     return (
         <nav class="navbar navbar-expand-lg border mb-4">
             <div class="container">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#">KHMER CODER</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
